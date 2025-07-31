9018d37f50fad6c1347d96272162b7926d30082f
Ben Noetzel-Kiers <167789286+bkiers98@users.noreply.github.com>
2025-07-31 18:28:14 +0000

    Edit .js
    Add single round logic and score variables to .js

--- script.js
+++ script.js
@@ -1,41 +1,86 @@
 /* console.log("Hello, World!"); */
 
-function getComputerChoice(){
+/* score variables */
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
     /* randomly returns "rock", "paper", or "scissors" */
 
     /* generate random integer between 1-3 incl. */
     let randInt = Math.floor(Math.random() * 3) + 1;
+    let computerChoice;
+    /* assign meaning to random integer */
+    switch(randInt) {
+        case 1:
+            computerChoice = 'rock';
+            break;
+        case 2:
+            computerChoice = 'paper';
+            break;
+        case 3:
+            computerChoice = 'scissors';
+            break;
+    }
 
-    /* assign meaning to random integer 
-    1 = 'rock', 2 = 'paper', 3 = 'scissors' */
-
-    return randInt;
+    return computerChoice;
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     /* returns "rock", "paper", or "scissors" based on user input */
 
     /* prompt user for input */
-    let str1 = prompt("Throw rock, paper, or scissors to beat the machine! ");
-    let humanChoice;
-
-    /* assign meaning to user input */
-    switch(str1) {
-        case 'rock':
-            humanChoice = 1;
-            break;
-        case 'paper':
-            humanChoice = 2;
-            break;
-        case 'scissors':
-            humanChoice = 3;
-            break;
-        default:
-            return("Not a valid choice!");
-    }
+    let humanChoice = prompt("Throw rock, paper, or scissors to beat the machine! ");
 
     return humanChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    
+    /* initialize variables for final string return */
+    let winner;
 
-console.log(getHumanChoice());
+    /* compare strings to see who wins the round */
+    if (computerChoice == 'rock') {
+        if (humanChoice == 'paper') {
+            winner = 'You win! Paper beats Rock';
+        }
+        else if (humanChoice == 'scissors') {
+            winner = 'You lose! Rock beats Scissors';
+        }
+        else {
+            winner = 'Tie! Rock and Rock';
+        }
+    }
+    else if (computerChoice == 'paper') {
+        if (humanChoice == 'rock') {
+            winner = 'You lose! Paper beats Rock';
+        }
+        else if (humanChoice == 'scissors') {
+            winner = 'You win! Scissors beats Paper';
+        }
+        else {
+            winner = 'Tie! Paper and Paper';
+        }
+    }
+    else {
+        if (humanChoice == 'paper') {
+            winner = 'You lose! Scissors beats Paper';
+        }
+        else if (humanChoice == 'rock') {
+            winner = 'You win! Rock beats Scissors';
+        }
+        else {
+            winner = 'Tie! Scissors and Scissors';
+        }
+    }
+
+    console.log(winner);
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
